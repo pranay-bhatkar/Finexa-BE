fd479aa0cf3f4a13ac0f9b5cddf0fe8bbd21555e
pranay-bhatkar <pranaybhatkar81@gmail.com>
2025-11-22 00:33:21 +0530

    implemented the categoryMapper

---
+++ src/main/java/com/expense_tracker/utility/mapper/CategoryMapper.java
@@ -0,0 +1,26 @@
+package com.expense_tracker.utility.mapper;
+
+import com.expense_tracker.model.Category;
+
+public class CategoryMapper {
+//    public static Category toEntity(CategoryRequest dto) {
+//        if(dto == null) return null;
+//
+//        Category category = new Category();
+//
+//        category.setName(dto.getName());
+//        category.setType(dto.getType());
+//        return category;
+//    }
+//
+//
+//    public static CategoryResponseDTO toDTO(Category category){
+//        if(category == null) return null;
+//
+//        return new CategoryResponseDTO(
+//                category.getId(),
+//                category.getName(),
+//                category.getType()
+//        );
+//    }
+}
